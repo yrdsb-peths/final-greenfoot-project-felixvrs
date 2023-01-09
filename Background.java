db2d094d460f6a1a0a834f7870c5e5409800d2ad
felix <59274976+felixvrs@users.noreply.github.com>
2023-01-09 13:21:12 -0500

    scrolling background (initial)
    added code for scrolling background by danpost (Greenfoot).

---
+++ Background.java
@@ -0,0 +1,41 @@
+import greenfoot.*;
+
+/**
+ * Scrolling background.
+ * 
+ * @author danpost (Greenfoot user)
+ * @version January 2023
+ */
+public class Background extends World
+{
+    private static final String bgImageName = "space.jpg";
+    private static final double scrollSpeed = 2.5;
+    private static final int picHeight = (new GreenfootImage(bgImageName)).getHeight();
+ 
+    private GreenfootImage bgImage, bgBase;
+    private int scrollPosition = 0;
+     
+    public Background()
+    {    
+        super(500, 650, 1);
+        setBackground(bgImageName);
+        bgImage = new GreenfootImage(getBackground());
+        bgBase = new GreenfootImage(getWidth(), picHeight);
+        bgBase.drawImage(bgImage, 0, 0);
+    }
+     
+    public void act()
+    {
+        scrollPosition -= scrollSpeed;
+        while(scrollSpeed > 0 && scrollPosition < -picHeight) scrollPosition += picHeight;
+        while(scrollSpeed < 0 && scrollPosition > 0) scrollPosition -= picHeight;
+        paint(scrollPosition);
+    }
+     
+    private void paint(int position)
+    {
+        GreenfootImage bg = getBackground();
+        bg.drawImage(bgBase, position, 0);
+        bg.drawImage(bgImage, position + picHeight, 0);
+    }
+}
